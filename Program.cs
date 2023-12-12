8f295f19578e78e68cb903ea86f001834812bb01
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:51:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä nimesi: ");
+        string nimi = Console.ReadLine();
+
+        Console.WriteLine($"Hei {nimi}!");
+    }
+}
